b2f809ebc004bf20d9c98bf457160295c0ac0556
hkhangus <huukhangtc@gmail.com>
2024-05-08 15:23:31 +0700

    implement token api

--- api/server.go
+++ api/server.go
@@ -31,13 +31,19 @@ func NewServer(config util.Config ,store db.Store) (*Server, error) {
 		store: 		store,
 		tokenMaker: tokenMaker,
 	}
-	router := gin.Default()
-
+	
 	if v,ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
@@ -46,8 +52,6 @@ func NewServer(config util.Config ,store db.Store) (*Server, error) {
 	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
-
-	return server, nil
 }
 
 // start runs the HTTP server on a specific address 
