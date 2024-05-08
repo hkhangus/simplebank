1416e8d0b760b7aacdb963fd61d5ced29bda9dbb
hkhangus <huukhangtc@gmail.com>
2024-05-08 10:31:35 +0700

    add token to api

--- api/user_test.go
+++ api/user_test.go
@@ -185,7 +185,7 @@ func TestCreateUserAPI(t *testing.T) {
 			tc.buildStubs(store)
 
 			// start test server and send request
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			// marshal body data to JSON
